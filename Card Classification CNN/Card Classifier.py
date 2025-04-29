752d2b97ff19b7f2fd533ad5dc65f7b48ce5dd1d
Alex Berg <123572908+berga4439@users.noreply.github.com>
2025-04-29 11:08:51 -0500

    Created training for CNN

---
+++ Card Classification CNN/Card Classifier.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from PIL import Image
+from tqdm import tqdm
+
+class CardSet(Dataset):
+    def __init__(self, csv_file, set_type, transform=None):
+        self.data = pd.read_csv(csv_file)
+        self.data = self.data[self.data["data set"] == set_type].reset_index(drop=True)
+        self.transform = transform
+        self.classes = sorted(self.data["card type"].unique())
+        self.class_to_idx = {cls: idx for idx, cls in enumerate(self.classes)}
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, item):
+        img_path = "./dataset/" + self.data.iloc[item]["filepaths"]
+        label = self.class_to_idx[self.data.iloc[item]["card type"]]
+        image = Image.open(img_path).convert('RGB')
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+])
+
+
+
+class CardCNN(nn.Module):
+    def __init__(self, num_classes=14):
+        super(CardCNN, self).__init__()
+
+        self.conv_block = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(64 * 28 * 28, 256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.conv_block(x)
+        x = self.classifier(x)
+        return x
+
+
+def trainNN(epochs=5, batch_size=16, lr=0.001, display_test_acc=False):
+    train_set = CardSet("./dataset/cards.csv", "train", transform=transform)
+    test_set = CardSet("./dataset/cards.csv", "test", transform=transform)
+
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
+    card_cnn = CardCNN()
+
+    cross_entropy = nn.CrossEntropyLoss()
+
+    optimizer = torch.optim.Adam(card_cnn.parameters(), lr=lr)
+
+    running_loss = 0.0
+    for epoch in range(epochs):
+        print(f"Epoch {epoch + 1} of {epochs}")
+        for _, (image, label) in enumerate(tqdm(train_loader)):
+            optimizer.zero_grad()
+            output = card_cnn(image)
+            loss = cross_entropy(output, label)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+        print(f"Running loss for epoch {epoch + 1}: {running_loss:.4f}")
+        running_loss = 0.0
+        if display_test_acc:
+            with torch.no_grad():
+                pass
+
+
+trainNN(epochs=5, display_test_acc=False)
