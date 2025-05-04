ff4feba277674325fdfe86c1163f4a51a41c41e2
Alex Berg <123572908+berga4439@users.noreply.github.com>
2025-05-04 18:04:42 -0500

    Cleaned up star code

--- Star Clustering/StarClustering.py
+++ Star Clustering/StarClustering.py
@@ -44,3 +44,40 @@ ax.set_ylabel('Radius')
 ax.set_zlabel('Temperature')
 
 plt.show()
+
+#dropping outliers to re cluster biggest cluster
+cluster_sizes = pd.Series(labels).value_counts()
+
+largest_cluster = cluster_sizes.idxmax()
+
+X_filtered = X[labels == largest_cluster]
+
+X_filtered -= np.average(X_filtered, axis=0)
+X_filtered /= np.std(X_filtered, axis=0)
+
+
+inertia = np.zeros(14)
+for i in range(1, 15):
+    kmeans = KMeans(n_clusters=i, n_init=25).fit(X_filtered)
+    inertia[i-1] = kmeans.inertia_
+#plotting "elbow" graph for best fit of new set
+plt.plot(np.arange(1, 15), inertia)
+plt.show()
+
+
+kmeans = KMeans(n_clusters=4, n_init=25)
+kmeans.fit(X_filtered)
+labels = kmeans.fit_predict(X_filtered)
+
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, projection='3d')
+scatter = ax.scatter(X_filtered[:, 0], X_filtered[:, 1], X_filtered[:, 2], c=labels, cmap='viridis', s=50)
+
+centers = kmeans.cluster_centers_
+ax.scatter(centers[:, 0], centers[:, 1], centers[:, 2], c='red', s=200, alpha=0.5, marker='d')
+
+ax.set_xlabel('Luminosity')
+ax.set_ylabel('Radius')
+ax.set_zlabel('Temperature')
+
+plt.show()
